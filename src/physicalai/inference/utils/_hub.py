9f2d0df48aa7efb23536dbec3ee7a15bfb22656b
Vlad Sovrasov <sovrasov.vlad@gmail.com>
2026-06-25 23:26:20 +0900

    Try to fix security

--- src/physicalai/inference/utils/_hub.py
+++ src/physicalai/inference/utils/_hub.py
@@ -57,14 +57,16 @@ def download_from_hub(
         )
         raise ImportError(msg) from exc
 
-    kwargs: dict[str, object] = {
-        "repo_id": repo_id,
-        "revision": revision,
-        "cache_dir": str(cache_dir) if cache_dir is not None else None,
-        "allow_patterns": allow_patterns,
-    }
+    kwargs: dict[str, object] = {}
     if token is not None:
         kwargs["token"] = token
 
-    local_path = snapshot_download(**kwargs)
+    local_path = snapshot_download(
+        repo_id=repo_id,
+        revision=revision,
+        cache_dir=str(cache_dir) if cache_dir is not None else None,
+        allow_patterns=allow_patterns,
+        **kwargs,
+    )
+
     return Path(local_path)
